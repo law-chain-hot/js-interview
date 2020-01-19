714a25002299843099de923a6e9347969c6d839c
qianhao <ianloe666@gmail.com>
2020-01-18 21:04:45 -0600

    flatten function by JavaScript

--- 5-1-arrFalt.js
+++ 5-1-arrFalt.js
@@ -7,3 +7,29 @@ function flat(arr) {
 
 arr = [[1,8,8,8,[10]], [3,9,[5]],6 ,7]
 console.log(flat(arr));
+
+
+
+/**
+ * Flattens passed array in one dimensional array
+ *
+ * @params {array} arr
+ * @returns {array}
+ */
+function flatten(arr) {
+    const result = [];
+  
+    arr.forEach((i) => {
+      if (Array.isArray(i))
+        result.push(...flatten(i));
+      else
+        result.push(i);
+    })
+    
+    return result;
+  }
+  
+  // Usage
+  const problem = [1, 2, 3, [4, 5, [6, 7], 8, 9]];
+  
+  flatten(problem); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
